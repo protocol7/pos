975a6b0cae26e07fe83e5c74985c206b1036983f
Niklas Gustavsson <niklas@protocol7.com>
2020-01-04 22:42:55 +0100

    Hacking on a RISC-V OS
    Based on http://osblog.stephenmarz.com/index.html

---
+++ setup-osx.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+brew install qemu
+
+rustup update
+rustup target add riscv64gc-unknown-none-elf
+rustup component add rustfmt
+
+dd if=/dev/zero of=hdd.dsk count=32 bs=1m
+
+brew tap riscv/riscv
+brew install riscv-tools
+
+install_name_tool -change '/usr/local/opt/isl/lib/libisl.21.dylib' /usr/local/opt/isl/lib/libisl.22.dylib /usr/local/Cellar/riscv-gnu-toolchain/gnu/libexec/gcc/riscv64-unknown-elf/9.2.0/cc1
